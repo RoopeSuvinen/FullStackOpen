c11ebe89a62f30041eb25438c75932eb9836e1ab
Roope Suvinen <roope.suvinen@gmail.com>
2024-12-12 08:36:46 +0200

    Toiminnallisuuden lisäämistä, all, avg..

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -9,7 +9,7 @@ const Button = (props) => {
 }
 
 const Statistics = (props) => {
-  return <h1>statistics</h1>
+
 }
 
 const App = () => {
@@ -17,6 +17,9 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(1)
+  const [average, setAvg] = useState(0)
+  const [positive, setPositive] = useState(0)
 
   return (
     <div>
@@ -24,10 +27,13 @@ const App = () => {
       <Button handleClick={() => setGood(good + 1)} text="good"/>
       <Button handleClick={() => setNeutral(neutral + 1)} text="neutral"/>
       <Button handleClick={() => setBad(bad + 1)} text="bad"/>
+      <h1>statistics</h1>
       <Statistics />
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {good + neutral + bad}</p>
+      <p>average {(good+neutral+bad)/all}</p>
     </div>
   )
 }
