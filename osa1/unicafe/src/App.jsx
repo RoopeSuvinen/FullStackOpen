0afeca970f2d1947918d43cfd5d671e384ff0f14
Roope <roope.suvinen@gmail.com>
2024-12-13 08:27:12 +0200

    Unicafe 1.7 tehtävä tehty

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -8,18 +8,14 @@ const Button = (props) => {
   return <button onClick={props.handleClick}>{props.text}</button>
 }
 
-const Statistics = (props) => {
-
-}
-
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [all, setAll] = useState(1)
-  const [average, setAvg] = useState(0)
-  const [positive, setPositive] = useState(0)
+  const total = good + neutral + bad
+  const average = total > 0 ? (good - bad) / total : 0
+  const positivePercentage = total > 0 ? (good / total) * 100 : 0
 
   return (
     <div>
@@ -28,12 +24,12 @@ const App = () => {
       <Button handleClick={() => setNeutral(neutral + 1)} text="neutral"/>
       <Button handleClick={() => setBad(bad + 1)} text="bad"/>
       <h1>statistics</h1>
-      <Statistics />
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
-      <p>all {good + neutral + bad}</p>
-      <p>average {(good+neutral+bad)/all}</p>
+      <p>all {total}</p>
+      <p>average {average.toFixed(2)}</p>
+      <p>positive {positivePercentage.toFixed(2)} %</p>
     </div>
   )
 }
