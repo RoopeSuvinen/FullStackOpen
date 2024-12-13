645b8cc5137305d5aec7b062fba084c1df9560bc
Roope <roope.suvinen@gmail.com>
2024-12-13 08:40:06 +0200

    1.8 Vaihe tehty, statistiikka laitettu omaan komponenttiin

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -8,21 +8,13 @@ const Button = (props) => {
   return <button onClick={props.handleClick}>{props.text}</button>
 }
 
-const App = () => {
-  // tallenna napit omaan tilaansa
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+const Statistics = ({good, neutral, bad}) => {
   const total = good + neutral + bad
   const average = total > 0 ? (good - bad) / total : 0
   const positivePercentage = total > 0 ? (good / total) * 100 : 0
-
+  
   return (
     <div>
-      <Header />
-      <Button handleClick={() => setGood(good + 1)} text="good"/>
-      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral"/>
-      <Button handleClick={() => setBad(bad + 1)} text="bad"/>
       <h1>statistics</h1>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
@@ -30,6 +22,23 @@ const App = () => {
       <p>all {total}</p>
       <p>average {average.toFixed(2)}</p>
       <p>positive {positivePercentage.toFixed(2)} %</p>
+    </div>  
+  )
+}
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <Header />
+      <Button handleClick={() => setGood(good + 1)} text="good"/>
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral"/>
+      <Button handleClick={() => setBad(bad + 1)} text="bad"/>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
