3bf3c06848ec04751dd178e5dccc334bc6deefef
Roope Suvinen <roope.suvinen@gmail.com>
2024-12-17 17:12:54 +0200

    2.8 puhelinluettelo step3. Lisätty puhelinnumerotoiminnallisuus. Lisäksi näytettävää listaa muokattu niin, että nimen perässä näkyy puhelinnumero. Tällä hetkellä puhelinnumero field hyväksyy myös kirjaimet.

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -2,9 +2,12 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-1233883' }
+  ])
+
+  // Hooks for setting states for name and number inputs
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
 /**
  * Event handler for adding a new name to a phonebook. Checks if
@@ -19,11 +22,13 @@ const addName = (event) => {
     return
   }
   const nameObject = {
-    name: newName
+    name: newName,
+    number: newNumber
   }
 
   setPersons(persons.concat(nameObject))
   setNewName('') // Clear input after adding a name
+  setNewNumber('')
 }
 
 /**
@@ -35,6 +40,11 @@ const handleAddName = (event) => {
   setNewName(event.target.value)
 }
 
+const handleAddNumber = (event) => {
+  console.log(event.target.value)
+  setNewNumber(event.target.value)
+}
+
   return (
     <div>
       <h2>Phonebook</h2>
@@ -42,6 +52,7 @@ const handleAddName = (event) => {
         <div>
           name: 
         <input value={newName} onChange={handleAddName}/>
+        <div>number: <input value={newNumber} onChange={handleAddNumber}/></div>
         </div>
         <div>
           <button type="submit">add</button>
@@ -50,7 +61,7 @@ const handleAddName = (event) => {
       <h2>Numbers</h2>
       <ul>
         {persons.map(person =>
-          <li key={person.name}>{person.name}</li>
+          <li key={person.name}> {person.name} {person.number}</li>
         )}
       </ul>
     </div>
