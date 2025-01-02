64544183fe1506058a657b66ebc5ab3b878f957c
Roope Suvinen <roope.suvinen@gmail.com>
2025-01-02 18:22:14 +0200

    3.3 puhelinluettelon backend step3. Yksittäisen puhelinnumerotiedon näyttämiseen liittyvä toiminnallisuus lisätty.

--- puhelinluettelo-backend/index.js
+++ puhelinluettelo-backend/index.js
@@ -43,6 +43,17 @@ app.get('/info', (request, response) => {
     response.send(infoText)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id // Id is string, not number.
+  const person = persons.find(person => person.id === id)
+  if (person) {
+    response.json(person)
+  } else {
+    console.log('Person not found')
+    response.status(404).end()
+  }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
