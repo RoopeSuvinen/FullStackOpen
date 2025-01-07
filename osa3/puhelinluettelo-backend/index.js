77c1dbbba4f7c9802b8c9afa6b0633ad2d0cd29f
Roope <roope.suvinen@gmail.com>
2025-01-07 21:26:24 +0200

    3.13 puhelinluettelo ja tietokanta, step1. Tietokantamäärittely eriytetty omaan moduuliin. Ympäristömuuttujat otettu käyttöön dotenv kirjastoa käyttäen.

--- osa3/puhelinluettelo-backend/index.js
+++ osa3/puhelinluettelo-backend/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 var morgan = require('morgan')
 const app = express()
@@ -86,7 +87,7 @@ app.post('/api/persons', (request, response) => {
 })
 
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
