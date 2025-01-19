1c74356eea2f1ad7808d312a9e52755f59017032
Roope Suvinen <roope.suvinen@gmail.com>
2025-01-19 22:54:42 +0200

    4.2 blogilista, step2, kesken. Sovellus jaettu useaan moduuliin.

---
+++ osa4/bloglista-backend/controllers/blogs.js
@@ -0,0 +1,46 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blogs')
+
+blogsRouter.get('/', (request, response) => {
+    Blog.find({}).then(blogs => {
+      response.json(blogs)
+    })
+  })
+
+  blogsRouter.get('/:id', (request, response, next) => {
+    Blog.findById(request.params.id)
+      .then(blog => {
+        if (blog) {
+          response.json(blog)
+        } else {
+          response.status(404).end()
+        }
+      })
+      .catch(error => next(error))
+  })
+
+  blogsRouter.post('/', (request, response, next) => {
+    const body = request.body
+  
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+    })
+
+    blog.save()
+    .then(savedBlog => {
+      response.json(savedBlog)
+    })
+    .catch(error => next(error))
+})
+
+blogsRouter.delete('/:id', (request, response, next) => {
+    Blog.findByIdAndDelete(request.params.id)
+      .then(() => {
+        response.status(204).end()
+      })
+      .catch(error => next(error))
+  })
+
+module.exports = blogsRouter
