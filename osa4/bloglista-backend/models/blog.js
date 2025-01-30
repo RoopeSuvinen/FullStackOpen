c575ecbdf67154fa86f3ad20ab3fe4fccc921277
Roope Suvinen <roope.suvinen@gmail.com>
2025-01-30 22:41:55 +0200

    4.15 blogilistan laajennus, step3. Lisätty backendiin toiminnallisuus jossa voidaan luoda käyttäjätunnus, salasana ja nimi HTTP POST -pyynnöllä. Lisäksi lisätty HTTP GET -pyyntö, jolla voidaan hakea tietokannasta käyttäjälista.

--- osa4/bloglista-backend/models/blog.js
+++ osa4/bloglista-backend/models/blog.js
@@ -18,6 +18,10 @@ const blogSchema = new mongoose.Schema({
   likes: {
     type: Number,
     default: 0
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
   }
 })
 
@@ -30,3 +34,28 @@ blogSchema.set('toJSON', {
 })
 
 module.exports = mongoose.model('Blog', blogSchema)
+
+const userSchema = mongoose.Schema({
+  username: String,
+  name: String,
+  passwordHash: String,
+  blogs: [
+    {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Blog',
+    }
+  ]
+})
+
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+    // The passwordHash should not be revealed
+    delete returnedObject.passwordHash
+  }
+})
+
+const User = mongoose.model('User', userSchema)
+module.exports = User
