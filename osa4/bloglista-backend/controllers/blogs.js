4f6b58a42dce41ef229434cfb45832d6abbdb291
Roope Suvinen <roope.suvinen@gmail.com>
2025-02-01 15:11:58 +0200

    4.17 blogilistan laajennus, step 5, kesken. Kontrollerin blogs.js post päivitetty sisältämään tiedot käyttäjän id:eestä

--- osa4/bloglista-backend/controllers/blogs.js
+++ osa4/bloglista-backend/controllers/blogs.js
@@ -1,50 +1,59 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({})
   response.json(blogs)
 })
 
 blogsRouter.get('/:id', async (request, response) => {
   const blog = await Blog.findById(request.params.id)
   if (blog) {
-    response.json(blog);
+    response.json(blog)
   } else {
     response.status(404).end()
   }
 })
 
 blogsRouter.post('/', async (request, response) => {
-  const { title, author, url, likes } = request.body
+  const body = request.body
+
+  const user = await User.findById(body.userId)
+
   const blog = new Blog({
-    title,
-    author,
-    url,
-    likes: likes || 0, // Default value 0.
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes || 0, // Default value 0.
+    user: user._id
   })
-  const savedBlog = await blog.save();
-  response.status(201).json(savedBlog);
-});
+
+  const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
+  response.status(201).json(savedBlog)
+})
 
 blogsRouter.delete('/:id', async (request, response) => {
-  await Blog.findByIdAndDelete(request.params.id);
-  response.status(204).end();
-});
+  await Blog.findByIdAndDelete(request.params.id)
+  response.status(204).end()
+})
 
 // Updates blogpost. Currently updates only likes. TODO: Like count wont
 // update to mongodb. Fix this.
 blogsRouter.put('/:id', async (request, response) => {
-  const { likes } = request.body;
+  const { likes } = request.body
   const updatedBlog = await Blog.findByIdAndUpdate(
     request.params.id,
     { likes }, // Updates likes
     { new: true, runValidators: true }
-  );
+  )
   if (!updatedBlog) {
-    return response.status(404).json({ error: 'Blog not found.' });
+    return response.status(404).json({ error: 'Blog not found.' })
   }
-  response.json(updatedBlog);
-});
+  response.json(updatedBlog)
+})
 
-module.exports = blogsRouter;
+module.exports = blogsRouter
