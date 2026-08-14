7fee497edbcb01a4b559ddb4ee5109d7ef2dfaa7
Roope Suvinen <roope.suvinen@gmail.com>
2026-08-14 14:53:48 +0300

    5.14 Blogilistan testit step2 päivitetty

--- osa4/blogilista/src/components/Blog.test.jsx
+++ osa4/blogilista/src/components/Blog.test.jsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react'
 import Blog from './Blog'
+import userEvent from '@testing-library/user-event'
 
 test('Renders blog title', () => {
   const blog = {
@@ -26,4 +27,40 @@ test('Renders blog title', () => {
   // Checks that blog title renders.
   const element = screen.getByText(/Component testing is done with react-testing-library/i)
   expect(element).toBeDefined()
+})
+
+test('Shows url, likes and user when view button is clicked', async () => {
+  const blog = {
+    id: '1',
+    title: 'Testiblogi',
+    author: 'Testikirjoittaja',
+    url: 'http://testi.com',
+    likes: 5,
+    user: {
+      id: 'user1',
+      username: 'MasaMainio',
+      name: 'Matti Meikäläinen'
+    }
+  }
+
+  const user = {
+    id: 'user1',
+    username: 'MasaMainio',
+    name: 'Matti Meikäläinen'
+  }
+
+  render(<Blog blog={blog} onVote={() => {}} onDelete={() => {}} user={user} />)
+
+  // Initially, url and likes should not be visible
+  expect(screen.queryByText(/http:\/\/testi.com/)).not.toBeInTheDocument()
+  expect(screen.queryByText(/Likes:/)).not.toBeInTheDocument()
+
+  // Click the view button
+  const viewButton = screen.getByRole('button', { name: 'view' })
+  await userEvent.click(viewButton)
+
+  // Now url, likes and user should be visible
+  expect(screen.getByText(/http:\/\/testi.com/)).toBeInTheDocument()
+  expect(screen.getByText(/Likes:/)).toBeInTheDocument()
+  expect(screen.getByText(/Added by:/)).toBeInTheDocument()
 })
