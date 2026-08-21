44a091767d448307787effb9f21c2a6c78aa6611
Roope Suvinen <roope.suvinen@gmail.com>
2026-08-21 13:17:29 +0300

    Lisätty helper.js tiedosto joka yksinkertaistaa testitiedostoa

--- osa4/blogilista/tests/blog_app.spec.js
+++ osa4/blogilista/tests/blog_app.spec.js
@@ -1,4 +1,5 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
+const { loginWith, createBlog } = require('./helper')
 
 describe('Blog app', () => {
   beforeEach(async ({ page, request }) => {
@@ -23,48 +24,30 @@ describe('Blog app', () => {
 
   describe('Login', () => {
     test('succeeds with correct credentials', async ({ page }) => {
-      await page.getByRole('textbox').nth(0).fill('roopesuvi')
-      await page.getByRole('textbox').nth(1).fill('secret')
-      await page.getByRole('button', { name: 'login' }).click()
+      await loginWith(page, 'roopesuvi', 'secret')
 
       await expect(page.getByText('Roope Suvinen logged in')).toBeVisible()
       await expect(page.getByRole('heading', { name: 'Add new blog' })).toBeVisible()
     })
 
     test('fails with wrong credentials', async ({ page }) => {
-      await page.getByRole('textbox').nth(0).fill('roopesuvi')
-      await page.getByRole('textbox').nth(1).fill('secretwrong')
-      await page.getByRole('button', { name: 'login' }).click()
+      await loginWith(page, 'roopesuvi', 'secretwrong')
 
       await expect(page.getByText('Wrong username or password')).toBeVisible()
       await expect(page.getByRole('heading', { name: 'Log in to application' })).toBeVisible()
     })
 
     test('a logged-in user can create a blog', async ({ page }) => {
-      await page.getByRole('textbox').nth(0).fill('roopesuvi')
-      await page.getByRole('textbox').nth(1).fill('secret')
-      await page.getByRole('button', { name: 'login' }).click()
-
-      await page.getByRole('button', { name: 'Show content' }).click()
-      await page.getByPlaceholder('Title').fill('Testiblogi')
-      await page.getByPlaceholder('Author').fill('Roope Suvinen')
-      await page.getByPlaceholder('URL').fill('https://example.com/testiblogi')
-      await page.getByRole('button', { name: 'Add blog' }).click()
+      await loginWith(page, 'roopesuvi', 'secret')
+      await createBlog(page, 'Testiblogi', 'Roope Suvinen', 'https://example.com/testiblogi')
 
       await expect(page.getByText('Testiblogi', { exact: true })).toBeVisible()
       await expect(page.getByText('A new blog "Testiblogi" by Roope Suvinen added')).toBeVisible()
     })
 
     test('a blog can be liked', async ({ page }) => {
-      await page.getByRole('textbox').nth(0).fill('roopesuvi')
-      await page.getByRole('textbox').nth(1).fill('secret')
-      await page.getByRole('button', { name: 'login' }).click()
-
-      await page.getByRole('button', { name: 'Show content' }).click()
-      await page.getByPlaceholder('Title').fill('Likeable blog')
-      await page.getByPlaceholder('Author').fill('Roope Suvinen')
-      await page.getByPlaceholder('URL').fill('https://example.com/likeable')
-      await page.getByRole('button', { name: 'Add blog' }).click()
+      await loginWith(page, 'roopesuvi', 'secret')
+      await createBlog(page, 'Likeable blog', 'Roope Suvinen', 'https://example.com/likeable')
 
       await page.getByRole('button', { name: 'View' }).click()
       const likeButton = page.getByRole('button', { name: 'like' })
@@ -74,15 +57,8 @@ describe('Blog app', () => {
     })
 
     test('a blog can be deleted', async({ page }) => {
-      await page.getByRole('textbox').nth(0).fill('roopesuvi')
-      await page.getByRole('textbox').nth(1).fill('secret')
-      await page.getByRole('button', { name: 'login' }).click()
-
-      await page.getByRole('button', { name: 'Show content' }).click()
-      await page.getByPlaceholder('Title').fill('Poistettava blogi')
-      await page.getByPlaceholder('Author').fill('Roope Suvinen')
-      await page.getByPlaceholder('URL').fill('https://example.com/poistettava')
-      await page.getByRole('button', { name: 'Add blog' }).click()
+      await loginWith(page, 'roopesuvi', 'secret')
+      await createBlog(page, 'Poistettava blogi', 'Roope Suvinen', 'https://example.com/poistettava')
 
       const blog = page.getByText('Poistettava blogi', { exact: true })
       await expect(blog).toBeVisible()
---
+++ osa4/blogilista/tests/helper.js
@@ -0,0 +1,16 @@
+const loginWith = async (page, username, password) => {
+  await page.getByRole('textbox').nth(0).fill(username)
+  await page.getByRole('textbox').nth(1).fill(password)
+  await page.getByRole('button', { name: 'login' }).click()
+}
+
+const createBlog = async (page, title, author, url) => {
+  await page.getByRole('button', { name: 'Show content' }).click()
+  await page.getByPlaceholder('Title').fill(title)
+  await page.getByPlaceholder('Author').fill(author)
+  await page.getByPlaceholder('URL').fill(url)
+  await page.getByRole('button', { name: 'Add blog' }).click()
+  await page.getByText(title, { exact: true }).waitFor()
+}
+
+export = { loginWith, createBlog }
